ccfb7751eea411b32914b341f106f96819f67ac5
root <root@localhost>
2024-11-24 21:16:11 -0500

    commit

---
+++ aws-iot-device-sdk-python-v2/documents/FAQ.md
@@ -0,0 +1,134 @@
+# Frequently Asked Questions
+
+*__Jump To:__*
+* [Where should I start](#where-should-i-start)
+* [How do I enable logging](#how-do-i-enable-logging)
+* [Installation Issues](#installation-issues)
+* [I keep getting AWS_ERROR_MQTT_UNEXPECTED_HANGUP](#i-keep-getting-aws_error_mqtt_unexpected_hangup)
+* [I am experiencing deadlocks](#i-am-experiencing-deadlocks)
+* [How do debug in VSCode?](#how-do-debug-in-vscode)
+* [What certificates do I need?](#what-certificates-do-i-need)
+* [I still have more questions about this sdk?](#i-still-have-more-questions-about-this-sdk)
+
+### Where should I start?
+
+If you are just getting started make sure you [install this sdk](https://github.com/aws/aws-iot-device-sdk-python-v2#installation) and then build and run the [basic PubSub](https://github.com/aws/aws-iot-device-sdk-python-v2/tree/main/samples#pubsub)
+
+### How do I enable logging?
+
+``` python
+io.init_logging(io.LogLevel.Error, 'stderr')
+```
+You can also enable [CloudWatch logging](https://docs.aws.amazon.com/iot/latest/developerguide/cloud-watch-logs.html) for IoT which will provide you with additional information that is not available on the client side sdk.
+
+### Installation Issues
+
+`awsiotsdk` depends on [awscrt](https://github.com/awslabs/aws-crt-python), which makes use of C extensions. Precompiled wheels are downloaded when installing on major platforms (Mac, Windows, Linux, Raspberry Pi OS). If wheels are unavailable for your platform, your machine must compile some C libraries. For example:
+
+```bash
+# 1. Create a workspace directory to hold all the CRT files
+mkdir crt-workspace
+cd crt-workspace
+
+# 2. Clone the repository, you could select the version you would like to use. You can find the awscrt 
+#    version used by the current SDK from the file "./aws-iot-device-sdk-python-v2/setup.py". Update 
+#    the version number in "./aws-iot-device-sdk-python-v2/setup.py" can change the awscrt version 
+#    you would like to use in awsiotsdk
+git clone -b <CRT_VERSION> https://github.com/awslabs/aws-crt-python.git
+
+# 3. Update the submodules
+cd aws-crt-python
+git submodule update --init --recursive
+
+# 4. (Optional) Setup the version number of your local build. Similar to the awsiotsdk, the default 
+#    version for awscrt is set to "1.0.0-dev", you can set the version number of the local build in 
+#    "./aws-crt-python/awscrt/__init__.py". The awscrt version set here need to match the version 
+#    specified in "./aws-iot-device-sdk-python-v2/setup.py" so that the awsiotsdk could locate the 
+#    correct awscrt library.
+sed -i "s/__version__ = '1.0.0.dev0'/__version__ = '<CRT_VERSION>'/" awscrt/__init__.py
+
+# 5. Install using Pip
+python3 -m pip install .
+```
+If you need aws-crt-python to use the libcrypto included on your system, set environment variable AWS_CRT_BUILD_USE_SYSTEM_LIBCRYPTO=1 while building from source:
+```
+AWS_CRT_BUILD_USE_SYSTEM_LIBCRYPTO=1 python3 -m pip install --no-binary :all: --verbose awscrt
+```
+( --no-binary :all: ensures you do not use the precompiled wheel from PyPI)
+
+If you encounter issues, see [Installation Issues](./PREREQUISITES.md#installation-issues) and try again.
+
+
+### I keep getting AWS_ERROR_MQTT_UNEXPECTED_HANGUP
+
+This could be many different things but it most likely is a policy issue. Start with using a super permissive IAM policy called AWSIOTFullAccess which looks like this:
+
+``` json
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "iot:*"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+```
+
+After getting it working make sure to only allow the actions and resources that you need. More info about IoT IAM policies can be found [here](https://docs.aws.amazon.com/iot/latest/developerguide/security_iam_service-with-iam.html).
+
+### I am experiencing deadlocks
+You MUST NOT perform blocking operations on any callback, or you will cause a deadlock. For example: in the on_publish_received callback, do not send a publish, and then wait for the future to complete within the callback. The Client cannot do work until your callback returns, so the thread will be stuck.
+
+### How do debug in VSCode?
+
+Here is an example launch.json file to run the pubsub sample
+ ``` json
+ {
+    // Use IntelliSense to learn about possible attributes.
+    // Hover to view descriptions of existing attributes.
+    // For more information, visit: https://go.microsoft.com/fwlink/?linkid=830387
+    "version": "0.2.0",
+    "configurations": [
+        {
+            "name": "PubSub",
+            "type": "python",
+            "request": "launch",
+            "program": "${workspaceFolder}/samples/pubsub.py",
+            "args": [
+                "--endpoint", "<account-number>-ats.iot.<region>.amazonaws.com",
+                "--ca_file", "<path to root-CA>",
+                "--cert", "<path to cert>",
+                "--key", "<path to key>",
+                "--client-id", "test-client"
+            ]
+        }
+    ]
+}
+```
+
+### What certificates do I need?
+
+* You can download pre-generated certificates from the AWS console (this is the simplest and is recommended for testing)
+* You can also generate your own certificates to fit your specific use case. You can find documentation for that [here](https://docs.aws.amazon.com/iot/latest/developerguide/x509-client-certs.html) and [here](https://iot-device-management.workshop.aws/en/provisioning-options.html)
+* Certificates that you will need to run the samples
+    * Root CA Certificates
+        * Download the root CA certificate file that corresponds to the type of data endpoint and cipher suite you're using (You most likely want Amazon Root CA 1)
+        * Generated and provided by Amazon. You can download it [here](https://www.amazontrust.com/repository/) or download it when getting the other certificates from the AWS console
+        * When using samples it can look like this: `--ca_file root-CA.crt`
+    * Device certificate
+        * Intermediate device certificate that is used to generate the key below
+        * When using samples it can look like this: `--cert abcde12345-certificate.pem.crt`
+    * Key files
+        * You should have generated/downloaded private and public keys that will be used to verify that communications are coming from you
+        * When using samples you only need the private key and it will look like this: `--key abcde12345-private.pem.key`
+
+### I still have more questions about this sdk?
+
+* [Here](https://docs.aws.amazon.com/iot/latest/developerguide/what-is-aws-iot.html) are the AWS IoT Core docs for more details about IoT Core
+* [Here](https://docs.aws.amazon.com/greengrass/v2/developerguide/what-is-iot-greengrass.html) are the AWS IoT Greengrass v2 docs for more details about greengrass
+* [Discussion](https://github.com/aws/aws-iot-device-sdk-python-v2/discussions) questions are also a great way to ask other questions about this sdk.
+* [Open an issue](https://github.com/aws/aws-iot-device-sdk-python-v2/issues) if you find a bug or have a feature request
---
+++ aws-iot-device-sdk-python-v2/samples/edenscope_trigger_camera_capture.py
@@ -0,0 +1,206 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0.
+
+
+import datetime
+import boto3
+import glob
+import json
+import os
+import subprocess
+import sys
+import threading
+import time
+from awscrt import mqtt, http, io
+from awsiot import mqtt_connection_builder
+from botocore.exceptions import NoCredentialsError, ClientError
+
+
+# Parameters - Replace these with your actual values
+ENDPOINT = "ap4q7pw1ovbow-ats.iot.us-east-1.amazonaws.com"            # Endpoint e.g., "your-endpoint-ats.iot.region.amazonaws.com"
+CLIENT_ID = "EdenscopeRaspberryPiCameraClient"
+PATH_TO_CERT = os.path.expanduser("~/certs/device.pem.crt")           # Path to your device certificate
+PATH_TO_KEY = os.path.expanduser("~/certs/private.pem.key")           # Path to your private key
+PATH_TO_ROOT = os.path.expanduser("~/certs/Amazon-root-CA-1.pem")     # Path to the root CA certificate
+TOPIC = "edenscope/camera-capture-photo"                              # The MQTT topic to subscribe to
+
+
+# Callback when connection is accidentally lost.
+def on_connection_interrupted(connection, error, **kwargs):
+    print("Connection interrupted. error: {}".format(error))
+
+
+# Callback when an interrupted connection is re-established.
+def on_connection_resumed(connection, return_code, session_present, **kwargs):
+    print("Connection resumed. return_code: {} session_present: {}".format(return_code, session_present))
+    if return_code == mqtt.ConnectReturnCode.ACCEPTED and not session_present:
+        print("Session did not persist. Resubscribing to existing topics...")
+        resubscribe_future, _ = connection.resubscribe_existing_topics()
+        # Cannot synchronously wait for resubscribe result because we're on the connection's event-loop thread,
+        # evaluate result with a callback instead.
+        resubscribe_future.add_done_callback(on_resubscribe_complete)
+
+
+def on_resubscribe_complete(resubscribe_future):
+    resubscribe_results = resubscribe_future.result()
+    print("Resubscribe results: {}".format(resubscribe_results))
+    for topic, qos in resubscribe_results['topics']:
+        if qos is None:
+            sys.exit("Server rejected resubscribe to topic: {}".format(topic))
+
+
+# Callback when the subscribed topic receives a message
+def on_message_received(topic, payload, dup, qos, retain, **kwargs):
+    print("Received message from topic '{}': {}".format(topic, payload))
+    try:
+        message = json.loads(payload.decode('utf-8'))
+        action = message.get('action')
+        if action == 'capture':
+            print("Triggering CapturePhoto...")
+            # Execute the CapturePhoto program
+            capture_photo_path = os.path.expanduser("~/Desktop/Sony Camera SDK/Sony_Camera_SDK_V1.13_Linux64_ARMv8_Edenscope/build/CapturePhoto")
+            image_directory = os.path.expanduser("~/Desktop/Sony Camera SDK/Sony_Camera_SDK_V1.13_Linux64_ARMv8_Edenscope/build/AmazonCloudS3Bucket")
+           
+            result = subprocess.run([capture_photo_path], capture_output=True, text=True)
+            if result.stderr:
+                print("CapturePhoto errors:\n", result.stderr)
+           
+            # Find the latest image file starting with 'EDN'
+            image_pattern = os.path.join(image_directory, 'EDN*')
+            list_of_files = glob.glob(image_pattern)
+            if not list_of_files:
+                print("No image files found matching pattern 'EDN*'")
+                return
+            latest_file = max(list_of_files, key=os.path.getctime)
+           
+            # Generate the new filename with timestamp
+            timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+            new_filename = f'edenscope_raw_image_{timestamp}.jpg'
+            new_image_path = os.path.join(image_directory, new_filename)
+           
+            # Rename the latest image file
+            try:
+                os.rename(latest_file, new_image_path)
+            except OSError as e:
+                print(f"Error renaming file: {e}")
+                return
+           
+            # Upload the image to S3
+            bucket_name = 'edenscope-raspberrypi-bucket'
+            if upload_to_s3(new_image_path, bucket_name, new_filename):
+                print(f"Successfully uploaded {new_image_path} to S3 bucket {bucket_name}.")
+            else:
+                print(f"Failed to upload {new_image_path} to S3 bucket {bucket_name}.")
+           
+        else:
+            print("Received unknown action: {}".format(action))
+    except Exception as e:
+        print("Error processing message: {}".format(e))
+
+
+
+
+# Callback when the connection successfully connects
+def on_connection_success(connection, callback_data):
+    assert isinstance(callback_data, mqtt.OnConnectionSuccessData)
+    print("Connection Successful with return code: {} session present: {}".format(callback_data.return_code, callback_data.session_present))
+
+
+
+
+# Callback when a connection attempt fails
+def on_connection_failure(connection, callback_data):
+    assert isinstance(callback_data, mqtt.OnConnectionFailureData)
+    print("Connection failed with error code: {}".format(callback_data.error))
+
+
+
+
+# Callback when a connection has been disconnected or shutdown successfully
+def on_connection_closed(connection, callback_data):
+    print("Connection closed")
+   
+# Upload s3 function
+def upload_to_s3(file_path, bucket_name, object_name=None):
+    # If S3 object_name was not specified, use the file name
+    if object_name is None:
+        object_name = os.path.basename(file_path)
+
+
+    # Create an S3 client
+    s3_client = boto3.client('s3')
+
+
+    try:
+        response = s3_client.upload_file(file_path, bucket_name, object_name)
+        return True
+    except FileNotFoundError:
+        print(f"The file {file_path} was not found.")
+        return False
+    except NoCredentialsError:
+        print("AWS credentials not available.")
+        return False
+    except ClientError as e:
+        print(f"ClientError: {e}")
+        return False
+
+
+if __name__ == '__main__':
+    # Initialize the MQTT connection
+    io.init_logging(getattr(io.LogLevel, io.LogLevel.Error.name), 'stderr')
+    # Create the proxy options if the data is present in cmdData
+    proxy_options = None
+    
+
+    # Create a MQTT connection from the command line data
+    mqtt_connection = mqtt_connection_builder.mtls_from_path(
+        endpoint=ENDPOINT,
+        port=8883,
+        cert_filepath=PATH_TO_CERT,
+        pri_key_filepath=PATH_TO_KEY,
+        ca_filepath=PATH_TO_ROOT,
+        on_connection_interrupted=on_connection_interrupted,
+        on_connection_resumed=on_connection_resumed,
+        client_id=CLIENT_ID,
+        clean_session=False,
+        keep_alive_secs=30,
+        http_proxy_options=proxy_options,
+        on_connection_success=on_connection_success,
+        on_connection_failure=on_connection_failure,
+        on_connection_closed=on_connection_closed)
+
+
+    print("Connecting to {} with client ID '{}'...".format(ENDPOINT, CLIENT_ID))
+    connect_future = mqtt_connection.connect()
+
+
+    # Future.result() waits until a result is available
+    connect_future.result()
+    print("Connected!")
+   
+    # Subscribe
+    print("Subscribing to topic '{}'...".format(TOPIC))
+    subscribe_future, packet_id = mqtt_connection.subscribe(
+        topic=TOPIC,
+        qos=mqtt.QoS.AT_LEAST_ONCE,
+        callback=on_message_received)
+
+
+    subscribe_result = subscribe_future.result()
+    print("Subscribed with {}".format(str(subscribe_result['qos'])))
+
+
+    # Keep the script running to listen for messages
+    try:
+        print("Listening for messages on topic '{}'...".format(TOPIC))
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Interrupted by user.")
+
+
+    # Disconnect
+    print("Disconnecting...")
+    disconnect_future = mqtt_connection.disconnect()
+    disconnect_future.result()
+    print("Disconnected!")
